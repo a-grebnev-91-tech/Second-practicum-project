cfdc5a0709cabfaf4366220461a2113185c097e6
a-grebnev-91-tech <a.grebnev.91.tech@yandex.ru>
2022-04-26 22:04:00 +0300

    feat: add time in Task

--- src/tasktracker/taskdata/Task.java
+++ src/tasktracker/taskdata/Task.java
@@ -1,9 +1,15 @@
 package tasktracker.taskdata;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class Task implements Cloneable{
 
+    private Duration duration;
     private long id;
     private TaskStatus status;
+    private LocalDateTime startTime;
     private String name;
     private String description;
 
@@ -17,23 +23,47 @@ public class Task implements Cloneable{
         this.status = TaskStatus.NEW;
     }
 
-    public Task(TaskStatus status, String name, String description){
+    //if users create task manually, its start time cannot be in the past.
+    public Task(String name, String description, LocalDateTime startTime, Duration duration) {
+        this(name, description);
+        if (startTime == null || duration == null) {
+            this.startTime = null;
+            this.duration = null;
+        } else if (isInPast(startTime)) {
+            throw new TaskInvalidException("Cannot create task with a start time in the past");
+        } else {
+            this.startTime = startTime;
+            this.duration = duration;
+        }
+    }
+
+    //constructor for loading task from anywhere. Start time can be in the past.
+
+    public Task(
+            long id,
+            TaskStatus status,
+            String name,
+            String description,
+            LocalDateTime startTime,
+            Duration duration
+    ) {
         this(name, description);
         if (status == null)
             throw new TaskInvalidException("Cannot create task with null status");
-        this.status = status;
-    }
-
-    public Task(long id, TaskStatus status, String name, String description) {
-        this(status, name, description);
         if (id < 0)
             throw new TaskInvalidException("Cannot create task with ID less than 0");
         this.id = id;
+        if (startTime == null || duration == null) {
+            this.startTime = null;
+            this.duration = null;
+        } else {
+            this.startTime = startTime;
+            this.duration = duration;
+        }
     }
-
     @Override
     public Task clone() {
-        return new Task(this.id, this.status, this.name, this .description);
+        return new Task(this.id, this.status, this.name, this .description, this.startTime, this.duration);
     }
 
     @Override
@@ -45,12 +75,19 @@ public class Task implements Cloneable{
 
         if (id != task.id) return false;
         if (status != task.status) return false;
-        if (name != null ? !name.equals(task.name) : task.name != null) return false;
-        return description != null ? description.equals(task.description) : task.description == null;
+        if (!Objects.equals(name, task.name)) return false;
+        if (!Objects.equals(description, task.description)) return false;
+        if (!Objects.equals(startTime, task.startTime)) return false;
+        return Objects.equals(duration, task.duration);
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public LocalDateTime getEndTime() {
+        if(startTime == null) return null;
+        return startTime.plus(duration);
     }
 
     public long getID() {
@@ -65,12 +102,18 @@ public class Task implements Cloneable{
         return status;
     }
 
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
     @Override
     public int hashCode() {
         int result = (int) (id ^ (id >>> 32));
         result = 31 * result + (status != null ? status.hashCode() : 0);
         result = 31 * result + (name != null ? name.hashCode() : 0);
         result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (startTime != null ? startTime.hashCode() : 0);
+        result = 31 * result + (duration != null ? duration.hashCode() : 0);
         return result;
     }
 
@@ -93,6 +136,12 @@ public class Task implements Cloneable{
             this.status = status;
     }
 
+    public void setStartTime(LocalDateTime time) {
+        if (time == null) return;
+        if (isInPast(time)) throw new UnsupportedOperationException("Cannot set a start time in the past");
+        this.startTime = time;
+    }
+
     @Override
     public String toString() {
         return "Task{" +
@@ -101,5 +150,9 @@ public class Task implements Cloneable{
                 ", name='" + name + '\'' +
                 ", description.length()='" + (description != null ? description.length() : null) + '\'' +
                 '}';
+    }
+
+    private boolean isInPast(LocalDateTime startTime) {
+        return startTime.isBefore(LocalDateTime.now());
     }
 }
