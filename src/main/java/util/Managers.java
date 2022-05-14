e52e3381e56b6e63ec9ccd5b29bcc9decdb59f8b
a-grebnev-91-tech <a.grebnev.91.tech@yandex.ru>
2022-05-14 18:18:52 +0300

    style: little fix

--- src/main/java/util/Managers.java
+++ src/main/java/util/Managers.java
@@ -1,16 +1,13 @@
 package util;
 
-import manager.FileBackedTaskManager;
 import manager.HttpTaskManager;
 import manager.TaskManager;
 
-import java.io.File;
-
 public final class Managers {
-    private static final String uri = "http://localhost:8078";
+    private static final String URI = "http://localhost:8078";
     private Managers() {}
 
     public static TaskManager getDefault() {
-        return new HttpTaskManager(uri);
+        return new HttpTaskManager(URI);
     }
 }
